fe15ac8265596e1c1c35e1c1d2243791ef9096e5
KimmyRP19 <Kimkpm7@gmail.com>
2023-09-22 10:48:55 -0600

    First commit

---
+++ src/pkg2023_ti302_mptd/Main.java
@@ -0,0 +1,17 @@
+
+package pkg2023_ti302_mptd;
+
+/**
+ *
+ * @author Kimberly Perez Martinez
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/tools/Coordinate.java
@@ -0,0 +1,29 @@
+package tools;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Coordinate {
+    public static final float XMIN = -10.0f;
+    public static final float XMAX = 20.0f;
+    public static final float YMIN = -10.0f;
+    public static final float YMAX = 20.0f;
+    public static final int WIDTH = 300;
+    public static final int HEIGHT = 300;
+    
+    public static final int toScreenX(float x){
+        return (int) ( ( XMIN - x ) / (XMIN - XMAX ) * WIDTH);
+    }
+    public static final int toScreenY(float y){
+        return (int) ( (XMAX - y) / ( YMAX - YMIN) * HEIGHT);
+    }
+    
+    
+    
+}
